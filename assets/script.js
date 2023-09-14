7dba4bf0d19ae3abc88f3cabf2351600ade15a32
caldardn <caldardn@gmail.com>
2023-09-13 23:57:51 -0400

    Adding additionalJavaScript that changes the question and answers when user select start and an answer

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,69 @@
 var questionsBank = [
-  {
-    question:
-      "What is the output of the following code? console.log(typeof null);",
-    selection: [
-      { textA: "A. null", answer: false },
-      { textB: "B. undefined", answer: false },
-      { textC: "C. object", answer: true },
-      { textD: "D. number", answer: false },
-    ],
-  },
-
-  {
-    question: "What is a string in JavaScript?",
-    selection: [
-      { textA: "A. stored data that can be used later on", answer: false },
-      { textB: "B. It runs a block of code until specific conditions are met", answer: false },
-      { textC: "C. A a sequence of characters that can include letters, numbers, symbols, and spaces", answer: true },
-      { textD: "D. None of the above", answer: false },
-    ],
-  },
-
-  {
-    question: "Why is JavaScript important for web development?",
-    selection: [
-      { textA: "A. Allows to make dynamic and interactive web pages", answer: true },
-      { textB: "B. Helps control the layout, formatting, and appearance of web pages", answer: false },
-      { textC: "C. It is the structure of the webpage", answer: false },
-      { textD: "D. JavaScript is not important to web development", answer: false },
-    ],
-  },
-
-  {
-    question:
-      "Which of the following operators is a strict equal in JavaScript?",
-    selection: [
-      { textA: "A. ||", answer: false },
-      { textB: "B. ==", answer: false },
-      { textC: "C. <=", answer: false },
-      { textD: "D. ===", answer: true },
-    ],
-  },
-
-  {
-    question: "What is an object in JavaScript?",
-    selection: [
-      { textA: "A. It's an ordered list of elements xontaining any data type", answer: false },
-      { textB: "B. A collection of properties containing primitive data", answer: true },
-      { textC: "A reusable piece of code that can be used to perform a specific task", answer: false },
-      { textD: "An action or occurrence that happens as a result of user interaction", answer: false },
-    ]
-  }
-];
+    {
+      question:
+        "What is the output of the following code? console.log(typeof null);",
+      selection: [
+        { choice: "A. null", answer: false },
+        { choice: "B. undefined", answer: false },
+        { choice: "C. object", answer: true },
+        { choice: "D. number", answer: false },
+      ],
+    },
+  
+    {
+      question: "What is a string in JavaScript?",
+      selection: [
+        { choice: "A. stored data that can be used later on", answer: false },
+        { choice: "B. It runs a block of code until specific conditions are met", answer: false },
+        { choice: "C. A a sequence of characters that can include letters, numbers, symbols, and spaces", answer: true },
+        { choice: "D. None of the above", answer: false },
+      ],
+    },
+  
+    {
+      question: "Why is JavaScript important for web development?",
+      selection: [
+        { choice: "A. Allows to make dynamic and interactive web pages", answer: true },
+        { choice: "B. Helps control the layout, formatting, and appearance of web pages", answer: false },
+        { choice: "C. It is the structure of the webpage", answer: false },
+        { choice: "D. JavaScript is not important to web development", answer: false },
+      ],
+    },
+  
+    {
+      question:
+        "Which of the following operators is a strict equal in JavaScript?",
+      selection: [
+        { choice: "A. ||", answer: false },
+        { choice: "B. ==", answer: false },
+        { choice: "C. <=", answer: false },
+        { choice: "D. ===", answer: true },
+      ],
+    },
+  
+    {
+      question: "What is an object in JavaScript?",
+      selection: [
+        { choice: "A. It's an ordered list of elements xontaining any data type", answer: false },
+        { choice: "B. A collection of properties containing primitive data", answer: true },
+        { choice: "A reusable piece of code that can be used to perform a specific task", answer: false },
+        { choice: "An action or occurrence that happens as a result of user interaction", answer: false },
+      ]
+    }
+  ];
 
 var timerEl = document.querySelector("#timer")
 var ruleEL = document.querySelector("#rule-container")
 var startButton = document.querySelector("#start-button")
 var questionContainer = document.querySelector("#question-container")
 var questionEl = document.querySelector("#question")
-var answerEl = document.querySelector(".btn")
+var answerEl = document.querySelector("#answer-btn")
+var nextEl = document.querySelector("#next-btn")
+var ansBtn = document.querySelector(".btn")
+index = 0
 
 
-startButton.addEventListener('click', startGame)
+
 
 function countDown() {
     var timeRemaining = 75
@@ -85,11 +88,44 @@ function startGame() {
     ruleEL.classList.add('hide')
     questionContainer.classList.remove('hide')
     countDown()
-    
+    currentQuestion()
 }
 
-function nextQuestion(){
+function currentQuestion(){
+    var selectedQuestion = questionsBank[index]
+    var questionNum = index + 1 
+    questionEl.textContent = questionNum + ". " + selectedQuestion.question
     
-
+    selectedQuestion.selection.forEach(function(selection) {
+        var button = document.createElement("button");
+        button.textContent = selection.choice;
+        button.classList.add("btn");
+        // ansBtn.textContent = selection.choice;
+        answerEl.appendChild(button);
+        if(selection.answer){
+            button.dataset.answer = selection.answer
+        }
+        button.addEventListener('click', chosenAnswer)
+      });
 }
-console.log(questionsBank[0].selection[3].textD)
+    
+ function chosenAnswer(event){
+    event.preventDefault()
+    var chosenButton = event.target
+    var correct = chosenButton.dataset.answer === "true"
+    if(correct){
+        chosenButton.classList.add("correct")
+    }if(!correct){
+        chosenButton.classList.add("incorrect")
+    }
+    index++
+    if(index < questionsBank.length){
+        currentQuestion()
+
+    }else{
+        
+    }
+ }
+ 
+ 
+ startButton.addEventListener('click', startGame)
