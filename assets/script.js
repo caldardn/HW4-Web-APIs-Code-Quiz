b345ee3454cdf2110f0376bb4a5a0815c8870ca9
caldardn <caldardn@gmail.com>
2023-09-14 22:55:45 -0400

    Adding new JavaScript that displays the question and correctly shows the user the answer choices for that question.

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ var questionsBank = [
       selection: [
         { choice: "A. stored data that can be used later on", answer: false },
         { choice: "B. It runs a block of code until specific conditions are met", answer: false },
-        { choice: "C. A a sequence of characters that can include letters, numbers, symbols, and spaces", answer: true },
+        { choice: "C. A sequence of characters that can include letters, numbers, symbols, and spaces", answer: true },
         { choice: "D. None of the above", answer: false },
       ],
     },
@@ -44,10 +44,10 @@ var questionsBank = [
     {
       question: "What is an object in JavaScript?",
       selection: [
-        { choice: "A. It's an ordered list of elements xontaining any data type", answer: false },
+        { choice: "A. It's an ordered list of elements containing any data type", answer: false },
         { choice: "B. A collection of properties containing primitive data", answer: true },
-        { choice: "A reusable piece of code that can be used to perform a specific task", answer: false },
-        { choice: "An action or occurrence that happens as a result of user interaction", answer: false },
+        { choice: "C. A reusable piece of code that can be used to perform a specific task", answer: false },
+        { choice: "D. An action or occurrence that happens as a result of user interaction", answer: false },
       ]
     }
   ];
@@ -55,18 +55,19 @@ var questionsBank = [
 var timerEl = document.querySelector("#timer")
 var ruleEL = document.querySelector("#rule-container")
 var startButton = document.querySelector("#start-button")
+var highScoreEl = document.querySelector("score-button")
 var questionContainer = document.querySelector("#question-container")
 var questionEl = document.querySelector("#question")
 var answerEl = document.querySelector("#answer-btn")
 var nextEl = document.querySelector("#next-btn")
 var ansBtn = document.querySelector(".btn")
 index = 0
-
+var timeRemaining = 75
 
 
 
 function countDown() {
-    var timeRemaining = 75
+   
     var timeInterval = setInterval(() => {
     
         if(timeRemaining > 1){
@@ -83,8 +84,6 @@ function countDown() {
 }
 
 function startGame() {
-    
- 
     ruleEL.classList.add('hide')
     questionContainer.classList.remove('hide')
     countDown()
@@ -95,17 +94,19 @@ function currentQuestion(){
     var selectedQuestion = questionsBank[index]
     var questionNum = index + 1 
     questionEl.textContent = questionNum + ". " + selectedQuestion.question
-    
+    answerEl.textContent = selectedQuestion.selection.choice
     selectedQuestion.selection.forEach(function(selection) {
         var button = document.createElement("button");
         button.textContent = selection.choice;
         button.classList.add("btn");
-        // ansBtn.textContent = selection.choice;
         answerEl.appendChild(button);
+        
         if(selection.answer){
             button.dataset.answer = selection.answer
+            
         }
         button.addEventListener('click', chosenAnswer)
+       
       });
 }
     
@@ -113,19 +114,27 @@ function currentQuestion(){
     event.preventDefault()
     var chosenButton = event.target
     var correct = chosenButton.dataset.answer === "true"
+
+    Array.from(answerEl.children).forEach(function(button) {
+      if (button !== chosenButton) {
+        button.disabled = true;
+      }
+    });
+
     if(correct){
         chosenButton.classList.add("correct")
     }if(!correct){
         chosenButton.classList.add("incorrect")
+        timeRemaining -= 5
     }
-    index++
-    if(index < questionsBank.length){
-        currentQuestion()
+    
+    if(questionsBank.length > 4){
+      index++
+      nextEl.addEventListener('click', currentQuestion)
 
-    }else{
-        
     }
+    
  }
  
- 
+
  startButton.addEventListener('click', startGame)
